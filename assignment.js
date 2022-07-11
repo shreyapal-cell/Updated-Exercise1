7d3efd92b1047f7416b641209e02111f93744515
shreyapal-cell <84048091+shreyapal-cell@users.noreply.github.com>
2022-07-11 01:08:08 -0400

    second commit

---
+++ assignment.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    alert("Hello!");
+  }
+  myFunction();
+
+function myFunction1(name){
+    alert ("Shreya");
+  }
+  myFunction1("name");
+
+ 
+ const task3Element = document.getElementById('task-3');
+ task3Element onclick = document.getElementById('task-3').innerHTML="myFunction()";
+ 
+
+ let str1 = "Hello";
+ let str2 ="My";
+ let str3 = "World";
+ function function3 ( Hello, My, World){
+     alert ("str1" + "" + "" + "str2" +"" + "str3");
+ }
+ function3 (str1, str2 , str3);
+
